f1e5d60378a7bef06ec2fb97fb988a80a39fe128
David Coats <david@getsafetycone.com>
2021-05-27 16:38:18 -0700

    Add Standard service aggregations library.

---
+++ source/R5T.D0065.Standard/Code/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using R5T.Dacia;
+using R5T.Lombardy;
+
+
+namespace R5T.D0065.Standard
+{
+    public static class IServiceCollectionExtensions
+    {
+        public static
+            (
+            IServiceAction<IExecutableDirectoryPathProvider> _,
+            IServiceAction<IExecutableFilePathProvider> IExecutableFilePathProviderAction
+            )
+        AddExecutableDirectoryPathProviderAction(this IServiceCollection services,
+            IServiceAction<IStringlyTypedPathOperator> stringlyTypedPathOperatorAction)
+        {
+            // Level 0.
+            var executableFilePathProviderAction = services.AddExecutableFilePathProviderAction();
+
+            // Level 1.
+            var executableDirectoryPathProviderAction = services.AddExecutableDirectoryPathProviderAction(
+                executableFilePathProviderAction,
+                stringlyTypedPathOperatorAction);
+
+            return
+                (
+                executableDirectoryPathProviderAction,
+                executableFilePathProviderAction);
+        }
+    }
+}
